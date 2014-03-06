be7ceeb36cd29ec581af8357d40ded378c166f4b
lincolnge <326684793@qq.com>
2014-03-06 11:32:00 +0800

    ask and answer questions

---
+++ testing/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+import "github.com/lincolnge/go-lang-func/math"
+import "runtime/pprof" // 引用pprof package
+import "os"
+
+func main() {
+	f, _ := os.Create("profilefile.prof")
+	pprof.StartCPUProfile(f)     // 开始cpu profile，结果写到文件f中
+	defer pprof.StopCPUProfile() // 结束profile
+
+	for i := 0; i < 100000; i++ {
+		xs := []float64{1, 2, 3, 4}
+		avg := math.Average(xs)
+		fmt.Println(avg)
+	}
+}
